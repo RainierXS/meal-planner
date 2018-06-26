29f0ca9e044cbbac04a99911fb840f023fff1986
Justin Rutherford <rutherfordjr@gmail.com>
2018-06-26 19:01:07 +0000

    nav menu start

--- react-client/src/layout/MenuBar.jsx
+++ react-client/src/layout/MenuBar.jsx
@@ -1,22 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const IconButton = styled.button`
+  border: 0;
+  background: none;
+  padding: 0;
+  margin: .5em;
+`;
+
+const FlexDiv = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  height: 55px;
+  background: darkviolet;
+  align-items: center;
+  border-bottom: 2px solid lime;
+  z-index: 10;
+`;
+
+const Header = styled.h2`
+  flex-grow: 1;
+  text-align: center;
+`;
+
+const MenuIcon = styled.i`
+  font-size: 2rem;
+  padding: 0;
+  margin: 0;
+  box-shadow: black 0px 0px 1px 0px;
+  vertical-align: middle;
+`;
 
 const MenuBar = (props) => {
   return (
-    <div>
-      <div>
-        <button
-          className=''
-          color="inherit"
-          aria-label="Menu"
-          onClick={() => props.showMenu('navDrawer')}
-        >
-          {/*<MenuIcon />*/}Menu
-        </button>
-        <h2 className=''>
-          Menu Planner!
-        </h2>
-      </div>
-    </div>
+    <FlexDiv>
+      <IconButton
+        className='icon-button'
+        color="inherit"
+        aria-label="Menu"
+        onClick={() => props.showMenu('navDrawer')}
+      >
+        <MenuIcon class="material-icons">menu</MenuIcon>
+      </IconButton>
+      <Header className=''>
+        Menu Planner
+      </Header>
+    </FlexDiv>
   )
 }
 
