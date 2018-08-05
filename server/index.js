18fcf5d3cccb683683aa1d8fe0df3d00988766fa
Justin Rutherford <rutherfordjr@gmail.com>
2018-08-05 12:25:43 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, '../react-client/dist')));
 
 // default page
 app.get('*', (req, res) => {
+  console.log('test')
   res.redirect('/');
 });
 // endregion express routes
